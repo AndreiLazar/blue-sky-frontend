abd4e44a99c81e03ce73e9526ced8cc38e61f13a
KRO\andrei.lazar <andrei.lazar@alten.com>
2022-04-05 16:35:52 +0300

    wip

--- src/app/core/services/user-management.service.ts
+++ src/app/core/services/user-management.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { User } from '../models/user.model';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,24 +12,38 @@ export class UserManagementService {
 
   constructor(private httpClient: HttpClient) { }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(() => new Error('Something bad happened; please try again later.'));
+  }
+  
   public getAllUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>(`${this.apiUrl}/all`);
+    return this.httpClient.get<User[]>(`${this.apiUrl}/all`).pipe(catchError(this.handleError));
   }
 
   public getUser(id): Observable<any> {
-    return this.httpClient.get(`${this.apiUrl}/${id}`);
+    return this.httpClient.get(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError));
   }
 
   public addUser(user): Observable<any> {
-    return this.httpClient.post(this.apiUrl, user);
+    return this.httpClient.post(this.apiUrl, user).pipe(catchError(this.handleError));
   }
 
   public editUser(user): Observable<any> {
-    return this.httpClient.put(this.apiUrl, user);
+    return this.httpClient.put(this.apiUrl, user).pipe(catchError(this.handleError));
   }
 
   public deleteUser(id) {
-    return this.httpClient.delete(this.apiUrl + '/' + id);
+    return this.httpClient.delete(this.apiUrl + '/' + id).pipe(catchError(this.handleError));
   }
 
 }
